7863e745b1ea4dcae7304ab7d56608508caf9ed3
Niket576 <niketchaudhary576@gmail.com>
2025-08-28 08:39:22 +0530

    problem in server.js with app.get and app.use

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,8 @@ app.use('/api/payments', paymentRoutes);
 app.use('/api/analytics', analyticsRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use("*", (req, res) => {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     });
 }
