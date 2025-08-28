8d6f8f7f0c13da8c34cab84cc5268730dbc32473
Niket576 <niketchaudhary576@gmail.com>
2025-08-28 08:18:40 +0530

    managing package.json file and the final step

---
+++ backend/routes/payment.route.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+import { createCheckoutSession, checkoutSuccess } from '../controllers/payment.controller.js';
+import { protectRoute } from '../middlewares/auth.middleware.js';
+
+const router = express.Router();
+
+router.post('/create-checkout-session', protectRoute, createCheckoutSession);
+router.post("/checkout-success", protectRoute, checkoutSuccess);
+
+export default router;
